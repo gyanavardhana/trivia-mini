8b820250988f208af6c40ac795ee817580f62e7b
gyanavardhana <gyanavardhanmamidisetti@gmail.com>
2024-11-17 13:00:13 +0530

    added styles,notifications

--- src/Components/Game/Game.jsx
+++ src/Components/Game/Game.jsx
@@ -1,11 +1,13 @@
 import { useState, useEffect } from "react";
 import { useLocation, useNavigate } from "react-router-dom";
 import { getCategories, getallQuestions } from "../../Services/Services";
-
+import Navbar from "../Home/Navbar";
+import { toast } from "react-toastify";
 const Game = () => {
   const location = useLocation();
+
   const navigate = useNavigate();
-  const { player1, player2 } = location.state;
+  const { player1, player2 } = location.state || {};
   const [categories, setCategories] = useState(null);
   const [questions, setQuestions] = useState(null);
   const [isOpen, setisOpen] = useState(false);
@@ -14,18 +16,16 @@ const Game = () => {
   const [player2score, setPlayer2score] = useState(0);
   const [currentIndex, setCurrentIndex] = useState(-1);
   const [selectedOption, setSelectedOption] = useState(null);
-
+  useEffect(() => {
+    if (!location.state) {
+      navigate("/");
+    }
+  }, []);
   const handleExit = () => {
     if (currentIndex !== -1 && currentIndex < questions.length - 1) {
-      alert(
-        "Please complete the category to exit the game"
-      );
+      toast.error("Please complete the category to exit the game");
       return;
     }
-    console.log({
-      player1: player1score,
-      player2: player2score,
-    })
     navigate("/endgame", {
       state: {
         name1: player1,
@@ -35,6 +35,7 @@ const Game = () => {
       },
     });
   };
+
   const calcScore = () => {
     const scores = {
       easy: 10,
@@ -42,25 +43,25 @@ const Game = () => {
       hard: 20,
     };
     if (
-      questions[currentIndex].player == "player1" &&
+      questions[currentIndex].player === "player1" &&
       selectedOption === questions[currentIndex].correctAnswer
     ) {
       setPlayer1score(
         player1score + scores[questions[currentIndex].difficulty]
       );
     } else if (
-      questions[currentIndex].player == "player2" &&
+      questions[currentIndex].player === "player2" &&
       selectedOption === questions[currentIndex].correctAnswer
     ) {
-      console.log(selectedOption, questions[currentIndex].correctAnswer);
       setPlayer2score(
         player2score + scores[questions[currentIndex].difficulty]
       );
     }
   };
+
   const handleNext = () => {
     if (!selectedOption) {
-      alert("select a option for going to next question");
+      toast.error("Select an option before moving to the next question.");
       return;
     }
     setSelectedOption(null);
@@ -68,7 +69,7 @@ const Game = () => {
     if (currentIndex < questions.length - 1) {
       setCurrentIndex(currentIndex + 1);
     } else {
-      alert("Category completed! Please select a new category.");
+      toast.error("Category completed! Please select a new category.");
       setQuestions(null);
       setSelectedCategory(null);
       setCurrentIndex(-1);
@@ -85,6 +86,7 @@ const Game = () => {
   };
 
   useEffect(() => {
+    
     const fetchCategories = async () => {
       try {
         const response = await getCategories();
@@ -95,10 +97,11 @@ const Game = () => {
     };
     fetchCategories();
   }, []);
+
   const categoryselected = async (e) => {
     try {
       if (currentIndex !== -1 && currentIndex < questions.length - 1) {
-        alert(
+        toast.error(
           "Please complete all the questions in the current category before selecting a new one."
         );
         setisOpen(false);
@@ -110,24 +113,16 @@ const Game = () => {
         return;
       }
       setSelectedCategory(cat);
+      toast.success("Game Started.")
       const response = await getallQuestions(cat);
-
       setCategories(
         categories.map((category) => {
           if (Object.keys(category)[0] === cat) {
-            return { [cat]: true }; // Mark the category as used
+            return { [cat]: true }; 
           }
           return category;
         })
       );
-
-      const falseCount = categories.filter(
-        (obj) => Object.values(obj)[0] === false
-      ).length;
-
-      if (falseCount === 0) {
-        console.log("All categories used. Game ends!");
-      }
 
       setCurrentIndex(0);
       setQuestions(response);
@@ -138,61 +133,97 @@ const Game = () => {
 
   return (
     <>
-      Game: {player1} vs {player2}
-      <label>Select the category for your questions: </label>
-      <select
-        name="categories"
-        multiple
-        size={isOpen ? 5 : 1}
-        onClick={handleToggle}
-        onChange={categoryselected}
-        disabled={questions && currentIndex !== questions.length - 1}
-      >
-        <option value="">{selectdCategory ? selectdCategory : "Select"}</option>
-
-        {categories &&
-          Object.values(categories).length > 0 &&
-          Object.values(categories).map(
-            (obj, index) =>
-              Object.values(obj)[0] === false && (
-                <option key={index} value={Object.keys(obj)[0]}>
-                  {Object.keys(obj)[0]}
-                </option>
-              )
-          )}
-      </select>
-      <div>
-        {questions && currentIndex !== -1 ? (
-          <div>
-            <p>{questions[currentIndex].player}</p>
-            <p>{questions[currentIndex].question.text}</p>
-            {questions[currentIndex].incorrectAnswers.map((answer, index) => {
-              return (
-                <div key={index}>
-                  <input
-                    type="radio"
-                    name="answer"
-                    value={answer}
-                    onChange={handleOptionSelection}
-                    checked={selectedOption === answer}
-                  />
-                  {answer}
-                </div>
-              );
-            })}
-            <button onClick={handleNext}>Next Question</button>
+      <Navbar />
+      <div className="min-h-screen  text-slate-700">
+        <div className="max-w-4xl mx-auto p-6">
+          <h1 className="text-center text-3xl font-bold mb-8">
+            Game: <span className="text-slate-700">{player1}</span> vs{" "}
+            <span className="text-slate-700">{player2}</span>
+          </h1>
+          <div className="mb-6">
+            <label className="block text-lg font-medium mb-2">
+              Select the category for your questions:
+            </label>
+            <select
+              name="categories"
+              multiple
+              size={isOpen ? 5 : 1}
+              onClick={handleToggle}
+              onChange={categoryselected}
+              disabled={questions && currentIndex !== questions.length - 1}
+              className="w-full p-2 border-2 border-slate-500 rounded-md focus:ring-2 focus:ring-slate-800 bg-slate-200 text-slate-800"
+            >
+              <option value="" >
+                {selectdCategory ? selectdCategory : "Select"}
+              </option>
+              {categories &&
+                Object.values(categories).length > 0 &&
+                Object.values(categories).map(
+                  (obj, index) =>
+                    Object.values(obj)[0] === false && (
+                      <option
+                        key={index}
+                        value={Object.keys(obj)[0]}
+                        className="bg-slate-200 text-slate-800  "
+                      >
+                        {Object.keys(obj)[0]}
+                      </option>
+                    )
+                )}
+            </select>
           </div>
-        ) : (
-          <p>
-            {categories
-              ? "Select a category to start."
-              : "Loading categories..."}
-          </p>
-        )}
+          <div>
+            {questions && currentIndex !== -1 ? (
+              <div className="bg-slate-800 text-slate-200 p-6 rounded-xl shadow-lg">
+                <p className="text-lg mb-4">
+                  Current Turn:{" "}
+                  <span className="font-bold text-slate-300">
+                    {questions[currentIndex].player === "player1"? player1: player2}
+                  </span>
+                </p>
+                <p className="text-xl font-semibold mb-4">
+                  {questions[currentIndex].question.text}
+                </p>
+                {questions[currentIndex].incorrectAnswers.map(
+                  (answer, index) => (
+                    <div key={index} className="mb-2">
+                      <label className="flex items-center gap-2">
+                        <input
+                          type="radio"
+                          name="answer"
+                          value={answer}
+                          onChange={handleOptionSelection}
+                          checked={selectedOption === answer}
+                          className="accent-slate-500 cursor-pointer"
+                        />
+                        {answer}
+                      </label>
+                    </div>
+                  )
+                )}
+                <button
+                  onClick={handleNext}
+                  className="mt-4 w-full bg-slate-600 text-slate-200 py-2 rounded-md hover:bg-slate-500 transition-all"
+                >
+                  Next Question
+                </button>
+              </div>
+            ) : (
+              <p className="text-lg text-center">
+                {categories
+                  ? "Select a category to start."
+                  : "Loading categories..."}
+              </p>
+            )}
+          </div>
+          <button
+            onClick={handleExit}
+            className="mt-6 w-full bg-slate-600 text-slate-200 py-2 rounded-md hover:bg-slate-500 transition-all"
+          >
+            Exit Game
+          </button>
+        </div>
       </div>
-      <button onClick={handleExit}>
-        Exit Game
-      </button>
     </>
   );
 };
--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -1,18 +1,24 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
+import { toast } from "react-toastify";
+import Navbar from "./Navbar";
+
 const Home = () => {
   const navigate = useNavigate();
   const [formData, setformData] = useState({
     player1: "",
     player2: "",
   });
+  useEffect(() =>{
+    toast.success("Welcome to TriviaGame");
+  },[])
   const handleChange = (e) => {
     const { name, value } = e.target;
     setformData({ ...formData, [name]: value });
   };
   const startGame = () => {
     if (formData.player1 === "" || formData.player2 === "") {
-      alert("Players names should not be empty");
+      toast.error("Players names should not be empty")
     } else {
       navigate("/game", {
         state: { player1: formData.player1, player2: formData.player2 },
@@ -22,23 +28,53 @@ const Home = () => {
   return (
     <>
       <div>
-        <label>Enter the player1 name: </label>
-        <input
-          id="player1"
-          name="player1"
-          value={formData.player1}
-          onChange={handleChange}
-          type="text"
-        ></input>
-        <label>Enter the player2 name: </label>
-        <input
-          id="player2"
-          name="player2"
-          type="text"
-          onChange={handleChange}
-          value={formData.player2}
-        ></input>
-        <button onClick={startGame}>Play the game</button>
+        <Navbar />
+        <div className="flex justify-center">
+          <div className="bg-slate-700 h-max lg:w-1/3 sm:w-1/2 rounded-2xl mt-24 p-8  ">
+            <h1 className="text-slate-200 text-center text-2xl font-bold mb-6">
+              Enter player names
+            </h1>
+            <form className="space-y-8 text-center">
+              <label
+                className="block text-left font-medium text-xl text-slate-200"
+                htmlFor="player1"
+              >
+                Player 1:
+              </label>
+              <input
+                id="player1"
+                name="player1"
+                value={formData.player1}
+                onChange={handleChange}
+                className="w-full p-2 rounded-md border-2 border-gray-700 focus:outline-none focus:ring-2 focus:ring-slate-200"
+                type="text"
+              />
+              <label
+                className="block text-left font-medium text-xl text-slate-200"
+                htmlFor="player2"
+              >
+                Player 2:
+              </label>
+              <input
+                id="player2"
+                name="player2"
+                type="text"
+                onChange={handleChange}
+                value={formData.player2}
+                className="w-full p-2 rounded-md border-2 border-gray-700 focus:outline-none focus:ring-2 focus:ring-slate-200"
+              />
+              <button
+                onClick={() => {
+                  startGame();
+                }}
+                type="button"
+                className="w-full bg-slate-600 text-slate-200 p-2 rounded-md hover:bg-slate-500 transition-all"
+              >
+                Play the Game
+              </button>
+            </form>
+          </div>
+        </div>
       </div>
     </>
   );
